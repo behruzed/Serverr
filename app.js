35e24fde29bf11e00453bcffbd4c8ccb9b29f7fd
Behruz Abdurahimov <behruzabdurahimovofficial@gmail.com>
2024-02-15 14:06:41 +0500

    sadfdf

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ require('dotenv').config()
 const app = express()
 
 // Connect to MongoDB
-// mongoose.connect(process.env.DB_local_link)
-mongoose.connect(process.env.DB_global_link)
+// mongoose.connect(process.env.DB_global_link)
+mongoose.connect(process.env.DB_local_link)
     .then(data => {
         if (data) {
             console.log("DB connected...");
@@ -31,16 +31,16 @@ app.use('/', require('./routes/index')) // correct
 app.use('/auth', require('./routes/auth/auntification')) // correct
 
 // Teacher panel
-app.use('/routeTeacher', token, checkTeacher, require('./routes/route/routeTeacher')) // correct
+// app.use('/routeTeacher', token, checkTeacher, require('./routes/route/routeTeacher')) // correct
 
 // Information for teacher and student. Also get, get one, cretae, update, delete teacher or student
-app.use('/admin', token, checkAdmin, require('./routes/path/admin')) // correct
-app.use('/teachers', token, checkTeacher, require('./routes/path/teachers')) // correct
-app.use('/students', token, checkStudent, require('./routes/path/students')) // correct
+// app.use('/admin', token, checkAdmin, require('./routes/path/admin')) // correct
+// app.use('/teachers', token, checkTeacher, require('./routes/path/teachers')) // correct
+// app.use('/students', token, checkStudent, require('./routes/path/students')) // correct
 
 // Information for teacher groups
-app.use('/groups', token, checkTeacher, require('./routes/path/groups')) // correct
+// app.use('/groups', token, checkTeacher, require('./routes/path/groups')) // correct
 
 // Connect to port
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => console.log(`Running: ${PORT}`))
